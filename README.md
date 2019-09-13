f14f7a14f8731d7acb9e5b6b3b4777909d39008b
Sayf Jawad <sayf4all@hotmail.com>
2019-09-13 15:59:28 +0200

    Forked from bsn-generator

---
+++ README.md
@@ -0,0 +1,38 @@
+# bsn-generator
+Burger Service Nummer Generator (Burger Service Nummer, Sofinummer) 
+
+#### Applicatie informatie
+
+Naam: bsn-generator
+Maintainer: Sayf jawad ([sayf4all@hotmail.com](mailto:sayf4all@hotmail.com))
+
+#### Technieken
+Dit project maakt gebruik van:
+
+```
+Java 8
+Maven
+```
+
+
+#### Applicatie lokaal starten
+
+Bouwen tegen JDK 8
+
+```
+mvn clean install
+```
+
+Bouwen tegen JDK 11
+
+```
+mvn clean install -Djava.version=11
+```
+
+De applicatie kan lokaal gestart worden met:
+
+```
+java -jar bsn-generator-1.0-SNAPSHOT.jar 
+```
+
+Of door gebruik te maken van een IDE (zoals eclipse of IntelliJ).
---
+++ src/test/java/nl/multicode/bsn/AppTest.java
@@ -0,0 +1,41 @@
+package nl.multicode.bsn;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    private App app;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        app = new App();
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void main_null_args() {
+        app.main(null);
+        assertEquals(10, outContent.toString().length());
+    }
+
+    @Test
+    public void main_no_args() throws ArrayIndexOutOfBoundsException {
+        app.main(new String[]{});
+        assertEquals(10, outContent.toString().length());
+    }
+}
